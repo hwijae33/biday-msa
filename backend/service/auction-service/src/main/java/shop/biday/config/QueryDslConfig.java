b68e956b267ab711ca71d251f131417112cda878
kimdohee58 <kimdohee58@gmail.com>
2024-10-07 17:35:41 +0900

    fix : 주석 제거

--- backend/service/auction-service/src/main/java/shop/biday/config/QueryDslConfig.java
+++ backend/service/auction-service/src/main/java/shop/biday/config/QueryDslConfig.java
@@ -6,7 +6,6 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-//import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @Configuration
 public class QueryDslConfig {
